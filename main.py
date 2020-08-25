229a334244fae9a7c0dee112b7fb239975d011bd
ncelebic <nick@celebic.net>
2020-08-25 09:38:31 -0400

    test

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 arr = os.listdir(".")
+print("Current dir")
+print(os.getcwd())
+print("Current dir listing")
 print(arr)
+
+import sys
+print("program arguments")
+print(sys.argv)
 
 from extfunc import *
 
